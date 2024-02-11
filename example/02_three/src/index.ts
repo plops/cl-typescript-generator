7b771a9a35779285f18b55dc2a2eca391de82715
Martin Kielhorn <wolpumba@gmail.com>
2024-02-11 11:22:16 +0100

    make black sphere bigger

--- example/02_three/src/index.ts
+++ example/02_three/src/index.ts
@@ -9,21 +9,33 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+function makeSphere(rad: number = 1, wireframe: boolean, color: number) {
+    const geometry = new THREE.SphereGeometry(rad, 32, 32);
+    const material = new THREE.MeshBasicMaterial(
+        {
+            color: color,
+            wireframe: wireframe
+        });
+    const sphere = new THREE.Mesh(geometry, material);
+   
+    return sphere;
+}
 
-const geometry = new THREE.SphereGeometry(1,32,32);
 
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-
-const sphere = new THREE.Mesh(geometry, material);
-
-scene.add(sphere);
+const s1 = makeSphere(1,true,0xffffff);
+const s2 = makeSphere(.99,false,0x000000);
+scene.add(s1);
+scene.add(s2);
 
 camera.position.z = 5;
 
+
 function animate() {
     requestAnimationFrame(animate);
-    sphere.rotation.x += 0.001;
-    sphere.rotation.y += 0.01;
+    s1.rotation.x += 0.001;
+    s1.rotation.y += 0.01;   
+    s2.rotation.x += 0.001;
+    s2.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
