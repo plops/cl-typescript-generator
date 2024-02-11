9ae6c9bd523d8f54c6a9c7bd4c07f1513a945b1e
Martin Kielhorn <wolpumba@gmail.com>
2024-02-11 10:31:32 +0100

    start adding html

--- example/02_three/src/index.ts
+++ example/02_three/src/index.ts
@@ -7,3 +7,24 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth,window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.SphereGeometry(1,32,32);
+
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+
+const sphere = new THREE.Mesh(geometry, material);
+
+scene.add(sphere);
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+    sphere.rotation.x += 0.001;
+    sphere.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+animate();
